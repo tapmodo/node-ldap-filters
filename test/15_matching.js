3699ff8c66373aecd3f4be668614eda4119a3448
Tapmodo <khallman@tapmodo.com>
2015-05-19 16:44:32 -0400

    Added pending tests

---
+++ test/15_matching.js
@@ -0,0 +1,13 @@
+var should = require('chai').should();
+var Filter = require('../index');
+
+describe('Matching',function(){
+  
+  it('simple values');
+  it('multi-valued keys');
+  it('values that require escaping');
+  it('basic approx test ("sounds like")');
+  it('complex filter and object match');
+
+});
+
---
+++ test/20_parsing.js
@@ -0,0 +1,11 @@
+var should = require('chai').should();
+var Filter = require('../index');
+
+describe('Parsing',function(){
+
+  it('parse small filter');
+  it('parse more complex filter');
+  it('perform a match against a parsed filter');
+
+});
+
