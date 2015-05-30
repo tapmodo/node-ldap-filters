d8f76b690f565b35eb73eee82a26b14736f61c96
Tapmodo <khallman@tapmodo.com>
2015-05-30 14:43:38 -0400

    Completed unit tests

--- test/15_matching.js
+++ test/15_matching.js
@@ -1,13 +1,133 @@
 var should = require('chai').should();
 var Filter = require('../index');
+var expect = require('chai').expect;
+//var assert = require('chai').assert;
 
 describe('Matching',function(){
   
-  it('simple values');
-  it('multi-valued keys');
-  it('values that require escaping');
-  it('basic approx test ("sounds like")');
-  it('complex filter and object match');
+  it('simple equality',function(done){
+    var filter = Filter.attribute('sn').equalTo('smith');
+    expect(filter.match({sn:'smith'})).to.be.true;
+    expect(filter.match({sn:'SMITH'})).to.be.true;
+    expect(filter.match({sn:'jones'})).to.be.false;
+    done();
+  });
+
+  it('multi-valued keys',function(done){
+    var filter = Filter.attribute('gn').equalTo('rick');
+    var data = { gn: [ 'Richard', 'Dick', 'Rick', 'Ricky' ] };
+    expect(filter.match(data)).to.be.true;
+    data = { gn: [ 'Thomas', 'Tom' ] };
+    expect(filter.match(data)).to.be.false;
+    done();
+  });
+
+  it('attribute presense',function(done){
+    var filter = Filter.attribute('sn').present();
+    expect(filter.match({sn:'smith'})).to.be.true;
+    expect(filter.match({sn:'jones'})).to.be.true;
+    expect(filter.match({gn:'jim'})).to.be.false;
+    done();
+  });
+
+  it('attribute contains value',function(done){
+    var filter = Filter.attribute('sn').contains('smith');
+    expect(filter.match({sn:'smith'})).to.be.true;
+    expect(filter.match({sn:'smith-jones'})).to.be.true;
+    expect(filter.match({sn:'McSmithers'})).to.be.true;
+    expect(filter.match({sn:'Jones'})).to.be.false;
+    done();
+  });
+
+  it('attribute ends with value',function(done){
+    var filter = Filter.attribute('sn').endsWith('smith');
+    expect(filter.match({sn:'Smith'})).to.be.true;
+    expect(filter.match({sn:'Jones-Smith'})).to.be.true;
+    expect(filter.match({sn:'Jensen-Smith-Jones'})).to.be.false;
+    done();
+  });
+
+  it('attribute starts with value',function(done){
+    var filter = Filter.attribute('sn').startsWith('smith');
+    expect(filter.match({sn:'Smith'})).to.be.true;
+    expect(filter.match({sn:'Smith-Jones'})).to.be.true;
+    expect(filter.match({sn:'Jones-Smith-Jensen'})).to.be.false;
+    done();
+  });
+
+  it('basic approx test ("sounds like")',function(done){
+    var filter = Filter.attribute('gn').approx('jenny');
+    expect(filter.match({gn:'Jeni'})).to.be.true;
+    expect(filter.match({gn:'Jenney'})).to.be.true;
+    expect(filter.match({gn:'Ricky'})).to.be.false;
+    done();
+  });
+
+  it('aggregate AND',function(done){
+    var filter = Filter.AND([
+      Filter.attribute('gn').equalTo('jenny'),
+      Filter.attribute('sn').startsWith('jensen')
+    ]);
+    expect(filter.match({gn:'Jenny',sn:'Jensen'})).to.be.true;
+    expect(filter.match({gn:'Jenny',sn:'Jensen-Smith'})).to.be.true;
+    expect(filter.match({gn:'Jerry',sn:'Jensen'})).to.be.false;
+    expect(filter.match({sn:'Jensen'})).to.be.false;
+    done();
+  });
+
+  it('aggregate OR',function(done){
+    var filter = Filter.OR([
+      Filter.attribute('gn').equalTo('jenny'),
+      Filter.attribute('sn').startsWith('jensen')
+    ]);
+    expect(filter.match({gn:'Jenny',sn:'Jensen'})).to.be.true;
+    expect(filter.match({gn:'Jenny'})).to.be.true;
+    expect(filter.match({sn:'Jensen'})).to.be.true;
+    expect(filter.match({gn:'Jerry',sn:'Jones'})).to.be.false;
+    expect(filter.match({})).to.be.false;
+    done();
+  });
+
+  it('negation (NOT)',function(done){
+    var filter = Filter.NOT(
+      Filter.attribute('sn').equalTo('jensen')
+    );
+    expect(filter.match({sn:'Jensen'})).to.be.false;
+    expect(filter.match({sn:'Jones'})).to.be.true;
+    expect(filter.match({})).to.be.true;
+    done();
+  });
+
+  it('values that require escaping',function(done){
+    var filter = Filter.attribute('info').equalTo('*(test)*');
+    expect(filter.match({info:'*(test)*'})).to.be.true;
+    expect(filter.match({info:'(test)'})).to.be.false;
+    done();
+  });
+
+  it('complex filter and object match',function(done){
+    var filter = Filter.AND([
+      Filter.attribute('active').equalTo('1'),
+      Filter.NOT(
+        Filter.attribute('objectClass').equalTo('inetMailObject')
+      ),
+      Filter.OR([
+        Filter.attribute('gn').equalTo('jenny'),
+        Filter.attribute('sn').startsWith('jensen')
+      ])
+    ]);
+
+    var data = { active: '1', gn: 'jenny' };
+    expect(filter.match(data)).to.be.true;
+
+    data.objectClass = [ 'person' ];
+    expect(filter.match(data)).to.be.true;
+
+    data.objectClass = [ 'person', 'inetMailObject' ];
+    expect(filter.match(data)).to.be.false;
+
+    done();
+  });
 
 });
 
--- test/20_parsing.js
+++ test/20_parsing.js
@@ -1,11 +1,45 @@
 var should = require('chai').should();
+var expect = require('chai').expect;
 var Filter = require('../index');
+var assert = require('chai').assert;
 
 describe('Parsing',function(){
 
-  it('parse small filter');
-  it('parse more complex filter');
-  it('perform a match against a parsed filter');
+  it('parse small filter',function(done){
+    var filter = '(sn=smith)';
+    var parsed = Filter.parse(filter);
+    parsed.toString().should.be.equal(filter);
+    done();
+  });
+
+  it('parse more complex filter',function(done){
+    var filter = '(&(sn=smith)(gn=john)(!(age=5)))';
+    var parsed = Filter.parse(filter);
+    parsed.toString().should.be.equal(filter);
+    done();
+  });
+
+  it('perform a match against a parsed filter',function(done){
+    var filter = '(&(sn=jensen)(gn=jenny))';
+    var parsed = Filter.parse(filter);
+    var data = { sn: 'Jensen', gn: 'Jenny' };
+    expect(parsed.match(data)).to.be.true;
+    done();
+  });
+
+  it('non-matching data does not match parsed filter',function(done){
+    var filter = '(&(sn=jensen)(gn=jenny))';
+    var parsed = Filter.parse(filter);
+    var data = { sn: 'Jensen' };
+    expect(parsed.match(data)).to.be.false;
+    done();
+  });
+
+  it('fails on incorrectly formed filter',function(done){
+    var filter = '(sn=smith';
+    assert.throws(function(){Filter.parse(filter);}, Error);
+    done();
+  });
 
 });
 
